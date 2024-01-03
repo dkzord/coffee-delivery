3174dd11bcee164cbee93a7a5c2c9ab6e92f915e
Fernando <comododk@gmail.com>
2024-01-03 16:01:54 -0300

    feat: theme default and global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { GlobalStyles } from './styles/global'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+
 function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
       <h1>corpo</h1>
-    </>
+      <button>aqui</button>
+    </ThemeProvider>
   )
 }
 
