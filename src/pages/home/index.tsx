40847fabe44bc5d9135b6fffcba7fd2d650c7ad0
Fernando <comododk@gmail.com>
2024-01-04 01:42:48 -0300

    feat: card component and update menu styling

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,4 @@
+import { Card } from '@/components/Card'
 import { Header } from '@/components/Header'
 import { Section } from './components/Section'
 
@@ -8,7 +9,19 @@ export const Home = () => {
     <div>
       <Header />
       <Section />
-      <h1>Home</h1>
+
+      <S.MenuContainer>
+        <h1>Nossos cafés</h1>
+
+        <S.MenuList>
+          <Card title="Mocaccino" />
+          <div style={{ background: 'green' }}>item 2</div>
+          <div style={{ background: 'green' }}>item 3</div>
+          <div style={{ background: 'green' }}>item 4</div>
+          <div style={{ background: 'green' }}>item 4</div>
+          <div style={{ background: 'green' }}>item 4</div>
+        </S.MenuList>
+      </S.MenuContainer>
     </div>
   )
 }
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -1,3 +1,22 @@
 import styled from 'styled-components'
 
-export const HomeContainer = styled.div``
+export const MenuContainer = styled.div`
+  width: 100%;
+  max-width: 1120px;
+  margin: 0 auto;
+  margin-top: 2rem;
+  padding-bottom: 6rem;
+
+  h1 {
+    font: ${(props) => props.theme.font.titleL};
+    color: ${(props) => props.theme.color.base.subtitle};
+  }
+`
+
+export const MenuList = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 2rem;
+
+  margin-top: 3.375rem;
+`
