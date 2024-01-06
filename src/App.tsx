dff8f66de6df9cdd48c520f4a1125d04d4d2c714
Fernando <comododk@gmail.com>
2024-01-06 15:34:08 -0300

    feat: create routing for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import { Home } from './pages/home'
 import { GlobalStyles } from './styles/global'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { CoffeeProvider } from '@/contexts/CoffeeProvider'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { AppRoutes } from './routes'
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
       <CoffeeProvider>
-        <Home />
+        <Router>
+          <AppRoutes />
+        </Router>
       </CoffeeProvider>
     </ThemeProvider>
   )
